c255473e350153d71380d7713cc11e4e82408fe0
jefferson.valente <jeffersonvalente1988@gmail.com>
2024-03-30 23:56:10 -0300

    removed: curls

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -66,22 +66,6 @@ resource "aws_instance" "my_ec2_instance" {
       "cd /opt/app",
       "sudo nohup gunicorn --log-level debug -b 0.0.0.0:8000 api:app &", # Execute a aplicação em segundo plano
       "sleep 15",
-      "curl -sv 0.0.0.0:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"alice@example.com","comment":"first post!","content_id":1}'",
-      "sleep 5",
-      "curl -sv 0.0.0.0:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"alice@example.com","comment":"ok, now I am gonna say something more useful","content_id":1}'",
-      "sleep 5",
-      "curl -sv 0.0.0.0:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"bob@example.com","comment":"I agree","content_id":1}'",
-      "sleep 5",
-      "curl -sv 52.7.5.16:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"bob@example.com","comment":"I guess this is a good thing","content_id":2}'",
-      "sleep 5",
-      "curl -sv 52.7.5.16:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"charlie@example.com","comment":"Indeed, dear Bob, I believe so as well","content_id":2}'",
-      "sleep 5",
-      "curl -sv 52.7.5.16:8000/api/comment/new -X POST -H 'Content-Type: application/json' -d '{"email":"eve@example.com","comment":"Nah, you both are wrong","content_id":2}'",
-      "sleep 5",
-      "curl -sv 52.7.5.16:8000/api/comment/list/1",
-      "sleep 5",
-      "curl -sv 52.7.5.16:8000/api/comment/list/2",
-      "sleep 5",
       "curl http://localhost:8000 && echo 'Application is up and running'"
     ]
   }
